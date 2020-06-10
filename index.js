c1c3b734de456b532b86f564feb7d90c2a2fb36e
kitaken55 <MiwaKentoDrums@yahoo.co.jp>
2020-06-10 13:29:55 +0900

    first init

---
+++ index.js
@@ -0,0 +1,14 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const messagesRouter = require('./routers/messages/route');
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+
+const app = express();
+app.use('/', messagesRouter);
+
+exports.api = functions
+    .region('asia-northeast1')
+    .https
+    .onRequest(app);
---
+++ routers/messages/route.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const router = express.Router();
+
+const endPoint = '/messages'
+
+// /messages のみの時
+router
+    .route(endPoint)
+    .get((req, res) => {
+        res.json({
+            message: "Called by the GET method"
+        });
+    })
+    .post((req, res) => {
+        res.json({
+            message: "Called by the POST method"
+        });
+    });
+
+// /messages/1 など
+router
+    .route(`${endPoint}/:id`)
+    .put((req, res) => {
+        res.json({
+            message: `Called by the PUT method. ID:${req.params.id}`
+        });
+    })
+    .delete((req, res) => {
+        res.json({
+            message: `Called by the DELETE method. ID:${req.params.id} `
+        });
+    });
+
+
+
+module.exports = router;
