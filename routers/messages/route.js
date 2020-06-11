64c1b9f3439e2f4da572e47a7935849ecc4e6116
kitaken55 <MiwaKentoDrums@yahoo.co.jp>
2020-06-11 12:08:33 +0900

    cloudFunctionとfirestoreの連携

--- routers/messages/route.js
+++ routers/messages/route.js
@@ -1,37 +1,102 @@
-const express = require('express');
+const express = require("express");
+const admin = require("firebase-admin");
+var serviceAccount = require("../../serviceAccountKey.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://test3-138e7.firebaseio.com",
+});
 
 const router = express.Router();
+const endPoint = "/messages";
 
-const endPoint = '/messages'
+const db = admin.firestore();
 
 // /messages のみの時
 router
-    .route(endPoint)
-    .get((req, res) => {
-        res.json({
-            message: "Called by the GET method"
+  .route(endPoint)
+  .get(async (req, res) => {
+    const messages = [];
+    try {
+      const querySnapshot = await db.collection("messages").get();
+      querySnapshot.forEach((doc) => {
+        messages.push({
+          id: doc.id,
+          ...doc.data(),
         });
-    })
-    .post((req, res) => {
-        res.json({
-            message: "Called by the POST method"
-        });
+      });
+    } catch (error) {
+      console.error(error, "@@@@@@@@@");
+    }
+    res.json({
+      message: "Called by the GET method",
+      messages,
     });
+  })
+  .post(async (req, res) => {
+    const { name, body } = req.body;
+    const createdAt = new Date().toISOString();
+
+    try {
+      const docRef = await db.collection("messages").add({
+        name,
+        body,
+        createdAt,
+      });
+      const docSnapshot = await docRef.get();
+      const createdMessage = {
+        id: docSnapshot.id,
+        ...docSnapshot.data(),
+      };
+      res.json({
+        message: "Called by the POST method",
+        data: createdMessage,
+      });
+    } catch (error) {}
+  });
 
 // /messages/1 など
 router
-    .route(`${endPoint}/:id`)
-    .put((req, res) => {
-        res.json({
-            message: `Called by the PUT method. ID:${req.params.id}`
-        });
-    })
-    .delete((req, res) => {
-        res.json({
-            message: `Called by the DELETE method. ID:${req.params.id} `
-        });
-    });
+  .route(`${endPoint}/:id`)
+  .put( async (req, res) => {
+    const id = req.params.id;
+    // const { name, body } = req.body;
+    const name = req.body.name;
+    const body = req.body;
 
+    const newData = {
+      name,
+      body,
+    };
 
+    try {
+      await db
+      .collection('messages')
+      .doc(id)
+      .update(newData);
+      res.json({
+        message: `Updated!! ID:${id}`,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: `何かエラーが起きたよ ID:${id}`,
+      });
+    }
+  })
+  .delete( async (req, res) => {
+    const id = req.params.id;
+    try {
+        await db
+        .collection('messages')
+        .doc(id)
+        .delete();
+        res.json({
+          message: `deleted!! ID:${id}`,
+        });
+      } catch (error) {
+        res.status(500).json({
+          message: `何かエラーが起きたよ ID:${id}`,
+        });
+      }
+  });
 
 module.exports = router;
